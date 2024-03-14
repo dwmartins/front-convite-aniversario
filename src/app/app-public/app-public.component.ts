21f0c0cc09b59c58712caf2c89044839c767dc5b
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2024-03-14 20:22:34 -0300

    feat: Configurando rotas iniciais

---
+++ src/app/app-public/app-public.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-app-public',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app-public.component.html'
+})
+export class AppPublicComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AppPublicComponent } from './app-public/app-public.component';
+import { HomeComponent } from './app-public/views/home/home.component';
+import { AppAdminComponent } from './app-admin/app-admin.component';
+import { DashboardComponent } from './app-admin/views/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Rotas publicas do app
+    {path: '', component: AppPublicComponent,
+        children: [
+            {path: '', component: HomeComponent}
+        ]
+    },
+
+    //Rotas privadas, apenas admin
+    {path: 'admin', component: AppAdminComponent, 
+        children: [
+            {path: '', component: DashboardComponent}
+        ]
+    },
+
+    // Rotas default
+    {path: '', redirectTo: '', pathMatch: 'full'},
+    {path: '**', redirectTo: ''}
+];
